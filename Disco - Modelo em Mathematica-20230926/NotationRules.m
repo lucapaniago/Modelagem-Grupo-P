088c3e5159967e73b53b47ed794863c17bd6c9a9
lucapaniago <104458015+lucapaniago@users.noreply.github.com>
2023-09-27 23:32:43 -0300

    Exercício 1 - item a, b, c
    Caros, segue o código dos itens a, b,c do exercício 1 do Ciclista. Abraços

---
+++ Disco - Modelo em Mathematica-20230926/NotationRules.m
@@ -0,0 +1,7 @@
+(* ::Package:: *)
+
+notation = {
+	\[Alpha]_'[t]-> OverDot[\[Alpha]],
+	Cos[\[Alpha]_[t]]-> Subscript[c, \[Alpha]], Sin[\[Alpha]_[t]]-> Subscript[s, \[Alpha]], Sec[\[Alpha]_[t]]-> 1/Subscript[c, \[Alpha]], Tan[\[Alpha]_[t]]-> Subscript[s, \[Alpha]]/Subscript[c, \[Alpha]], 
+	\[Alpha]_[t]-> \[Alpha]
+	};
