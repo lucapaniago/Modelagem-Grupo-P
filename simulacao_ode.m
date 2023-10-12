71974770e02d998868af9f32b4b11d397bf90d78
lucapaniago <104458015+lucapaniago@users.noreply.github.com>
2023-10-12 16:58:30 -0300

    Plots com erro e linearizado corrigido

--- simulacao_ode.m
+++ simulacao_ode.m
@@ -1,4 +1,4 @@
-%Item g)-------------------------------------------------------------------
+    %Item g)-------------------------------------------------------------------
 %Condição inicial C1
 
 tspan = 0:0.001:10;
@@ -15,6 +15,7 @@ y_01 = [0 0.2 0 0];
 
 %Equações linearizada e não linearizada para ode45
 figure(1)
+subplot(2,1,1);
 plot(t1,y1(:,1),"b")
 hold on
 plot(t2,y2(:,1),"r")
@@ -23,8 +24,31 @@ xlabel("tempo [s]")
 ylabel("\theta_1 [rad]")
 title("\theta_1 utilizando o Método de Runge-Kutta na condição C1")
 
-%Equações linearizada e não linearizada para ode23t
+subplot(2,1,2);
+plot(t1,y1(:,2),"b")
+hold on
+plot(t2,y2(:,2),"r")
+legend("Linearizada", "Não Linearizada")
+xlabel("tempo [s]")
+ylabel("\theta_2 [rad]")
+title("\theta_2 utilizando o Método de Runge-Kutta na condição C1")
+%Erro
 figure(2)
+subplot(2,1,1);
+plot(t1,abs(y1(:,1)-y2(:,1)),"r")
+xlabel("tempo [s]")
+ylabel("|\theta_1_{lin} - \theta_1_{Ñlin}| [rad]")
+title("Diferença de \theta_1 entre modelo linearizado e não linearizado para método de Range-Kutta (C1)")
+
+subplot(2,1,2);
+plot(t1,abs(y1(:,2)-y2(:,2)),"r")
+xlabel("tempo [s]")
+ylabel("|\theta_2_{lin} - \theta_2_{Ñlin}| [rad]")
+title("Diferença de \theta_2 entre modelo linearizado e não linearizado (C1)")
+
+%Equações linearizada e não linearizada para ode23t
+figure(3)
+subplot(2,1,1);
 plot(t3,y3(:,1),"b")
 hold on
 plot(t4,y4(:,1),"r")
@@ -32,9 +56,33 @@ legend("Linearizada", "Não Linearizada")
 xlabel("tempo [s]")
 ylabel("\theta_1 [rad]")
 title("\theta_1 utilizando o Método dos Trapézios na condição C1")
+subplot(2,1,2);
+plot(t3,y3(:,2),"b")
+hold on
+plot(t4,y4(:,2),"r")
+legend("Linearizada", "Não Linearizada")
+xlabel("tempo [s]")
+ylabel("\theta_2 [rad]")
+title("\theta_2 utilizando o Método dos Trapézios na condição C1")
+%Erro
+figure(4)
+subplot(2,1,1);
+plot(t1,abs(y3(:,1)-y4(:,1)),"r")
+xlabel("tempo [s]")
+ylabel("|\theta_1_{lin} - \theta_1_{Ñlin}| [rad]")
+title("Diferença de \theta_1 entre modelo linearizado e não linearizado (C1)")
+
+subplot(2,1,2);
+plot(t1,abs(y3(:,2)-y4(:,2)),"r")
+xlabel("tempo [s]")
+ylabel("|\theta_2_{lin} - \theta_2_{Ñlin}| [rad]")
+title("Diferença de \theta_2 entre modelo linearizado e não linearizado para Método dos Trapézios (C1)")
+
+
 
 %Equações linearizada para ode45 e ode23
-figure(3)
+figure(5)
+subplot(2,1,1);
 plot(t1,y1(:,1),"b")
 hold on
 plot(t3,y3(:,1),"r")
@@ -42,9 +90,32 @@ legend("Método de Runge-Kutta", "Método dos Trapézios")
 xlabel("tempo [s]")
 ylabel("\theta_1 [rad]")
 title("\theta_1 linearizado com diferentes métodos de integração na condição C1")
+subplot(2,1,2);
+plot(t1,y1(:,2),"b")
+hold on
+plot(t3,y3(:,2),"r")
+legend("Método de Runge-Kutta", "Método dos Trapézios")
+xlabel("tempo [s]")
+ylabel("\theta_2 [rad]")
+title("\theta_2 linearizado com diferentes métodos de integração na condição C1")
+
+%Erro
+figure(6)
+subplot(2,1,1);
+plot(t1,abs(y1(:,1)-y3(:,1)),"r")
+xlabel("tempo [s]")
+ylabel("|\theta_1_{RK} - \theta_1_{T}| [rad]")
+title("Diferença de \theta_1 linearizado entre Método de Range-Kutta e dos Trapézios (C1)")
+
+subplot(2,1,2);
+plot(t1,abs(y1(:,2)-y3(:,2)),"r")
+xlabel("tempo [s]")
+ylabel("|\theta_2_{RK} - \theta_2_{T}| [rad]")
+title("Diferença de \theta_2 linearizado entre Método de Range-Kutta e dos Trapézios (C1)")
 
 %Equações não linearizada para ode45 e ode23
-figure(4)
+figure(7)
+subplot(2,1,1);
 plot(t2,y2(:,1),"b")
 hold on
 plot(t4,y4(:,1),"r")
@@ -52,6 +123,28 @@ legend("Método de Runge-Kutta", "Método dos Trapézios")
 xlabel("tempo [s]")
 ylabel("\theta_1 [rad]")
 title("\theta_1 não linearizado com diferentes métodos de integração na condição C1")
+subplot(2,1,2);
+plot(t2,y2(:,2),"b")
+hold on
+plot(t4,y4(:,2),"r")
+legend("Método de Runge-Kutta", "Método dos Trapézios")
+xlabel("tempo [s]")
+ylabel("\theta_2 [rad]")
+title("\theta_2 não linearizado com diferentes métodos de integração na condição C1")
+
+%Erro
+figure(8)
+subplot(2,1,1);
+plot(t1,abs(y2(:,1)-y4(:,1)),"r")
+xlabel("tempo [s]")
+ylabel("|\theta_1_{RK} - \theta_1_{T}| [rad]")
+title("Diferença de \theta_1 não linearizado entre Método de Range-Kutta e dos Trapézios (C1)")
+
+subplot(2,1,2);
+plot(t1,abs(y2(:,2)-y4(:,2)),"r")
+xlabel("tempo [s]")
+ylabel("|\theta_2_{RK} - \theta_2_{T}| [rad]")
+title("Diferença de \theta_2 não linearizado entre Método de Range-Kutta e dos Trapézios (C1)")
 
 %Item h)-------------------------------------------------------------------
 %Condição inicial C2
@@ -81,7 +174,7 @@ V6 = -0.5*g.*(cos(y6(:,2))*l2*m2+cos(y6(:,1)).*l1*(m1+2*m2));
 E6 = T6 + V6;
 
 %Equações não linearizada para ode45 e ode23 no caso C2
-figure(5)
+figure(9)
 plot(t5,E5,"b")
 hold on
 plot(t6,E6,"r")
@@ -98,7 +191,7 @@ function dy1 = f1(t,y)
     lamb = 9/5;
     dy1_1 = y(3);
     dy1_2 = y(4);
-    dy1_3 = (3*(w_p)^2*(-1*(4+ 2*lamb)*y(1) + 3*lamb*y(2)))/(3 + 4*lamb);
+    dy1_3 = (3*(w_p)^2*(-1*(4+ 2*lamb)*y(1) + 3*y(2)))/(3 + 4*lamb);
     dy1_4 = (3*lamb*(w_p)^2*((6 + 3*lamb)*y(1) - 2*(3 + 1*lamb)*y(2)))/(3 + 4*lamb);
     dy1 = [dy1_1;dy1_2;dy1_3;dy1_4];
 end
