38a48fac45cc26b9fc35918231ab67e4c6389555
lucapaniago <104458015+lucapaniago@users.noreply.github.com>
2023-11-18 09:37:40 -0300

    Modelagem copia para jogar no matlab

--- state_space.m
+++
@@ -1,5 +0,0 @@
-    dy1 = y(3);
-    dy2 = y(4);
-    dy3 = (-3*((5 + 4*\[Lambda])*sin(y(1)) + 3*sin(y(1) - 2*y(2)))*(Subscript(\[Omega],p)*Subscript(\[Omega],p)))/(15 + 8*\[Lambda] - 9*cos(2*(y(1) - y(2)))) + (9*sin(2*(y(1) - y(2)))*(y(3)*y(3)))/(-15 - 8*\[Lambda] + 9*cos(2*(y(1) - y(2)))) - (6*sin(y(1) - y(2))*(y(4)*y(4)))/(\[Lambda]*(4*(3 + \[Lambda]) - 9*(cos(y(1) - y(2))*cos(y(1) - y(2)))));
-    dy4 = (-3*\[Lambda]*(-3*(2 + \[Lambda])*sin(2*y(1) - y(2)) + (6 + \[Lambda])*sin(y(2)))*(Subscript(\[Omega],p)*Subscript(\[Omega],p)))/(15 + 8*\[Lambda] - 9*cos(2*(y(1) - y(2)))) + (6*\[Lambda]*(3 + \[Lambda])*sin(y(1) - y(2))*(y(3)*y(3)))/(4*(3 + \[Lambda]) - 9*(cos(y(1) - y(2))*cos(y(1) - y(2)))) + (9*sin(2*(y(1) - y(2)))*(y(4)*y(4)))/(15 + 8*\[Lambda] - 9*cos(2*(y(1) - y(2))));
-
