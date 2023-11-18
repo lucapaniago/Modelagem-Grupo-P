d0cf238729efaebe52c47d64d717b307e97c2aec
lucapaniago <104458015+lucapaniago@users.noreply.github.com>
2023-11-18 18:26:12 -0300

    ode 45 atualizado

--- Simulink/modelagem_script.m
+++ Simulink/modelagem_script.m
@@ -1,8 +1,8 @@
 %%Script para simulação dinâmica e cinemática do Cubli
 clc
 clear
-b = 17.03*10^(-3);
-%b = 0;
+%b = 17.03*10^(-3);
+b = 0;
 Tau = [0;0;0];
 g = [0;0;-9.81]; 
 mc = 0.55;
@@ -12,7 +12,7 @@ e_s = [-sqrt(2)/2;sqrt(2)/2;0];
 e_u = [sqrt(2)/2;-sqrt(2)/2;0];
 
 q00 = [cos(phi_u/2);e_u*sin(phi_u/2)]; %posição de equilíbrio instável
-q00 =[cos(phi_s/2);e_s*sin(phi_s/2)]; %posição de equilíbrio estável
+%q00 =[cos(phi_s/2);e_s*sin(phi_s/2)]; %posição de equilíbrio estável
 %q00 = [1;0;0;0];
 %w00 = [0;0;0];
 w00 =2*pi/sqrt(3)*[1;1;1];
--- Trabalho Final - Cubli/state_space_cubli_simulation.m
+++ Trabalho Final - Cubli/state_space_cubli_simulation.m
@@ -1,11 +1,11 @@
 
-<<<<<<< HEAD
+%<<<<<<< HEAD
 tspan = [0 3];
 phi_u = acos(sqrt(3)/3);
 e_u = [sqrt(2)/2,-sqrt(2)/2,0];
-%q_0 = [cos(phi_u/2) e_u*sin(phi_u/2) 0 0 0];
+q_0 = [cos(phi_u/2) e_u*sin(phi_u/2) 0 0 0];
 %q_0 = [1 0 0 0 0 0 0];
-=======
+%=======
 tspan = 0:0.001:10;
 
 phi_s = acos(-sqrt(3)/3);
@@ -13,7 +13,7 @@ phi_u = acos(sqrt(3)/3);
 
 q_0s = [cos(phi_s/2) (-sqrt(2)/2)*sin(phi_s/2) (sqrt(2)/2)*sin(phi_s/2) 0 0 0 0];
 q_0u = [cos(phi_u/2) (sqrt(2)/2)*sin(phi_u/2) (-sqrt(2)/2)*sin(phi_u/2) 0 0 0 0];
->>>>>>> df85378a39f670aa288e4e30a1048dfd97ea968f
+
 ops = odeset('MaxStep', 1e-5);
 
 %Modelo não linear---------------------------------------------------------
@@ -32,7 +32,6 @@ plot(t1s,q1s(:,4))
 legend("q_0","q_1","q_2","q_3")
 xlabel("tempo (s)")
 ylabel("quaternions")
-<<<<<<< HEAD
 % q = [q1(:,1) q1(:,2)  q1(:,3) q1(:,4)];
 % figure(2)
 % for i = 1:length(q1(:,1))
@@ -40,14 +39,11 @@ ylabel("quaternions")
 %    hold on
 % end
 
-figure(3)
-plot(t1,q1(:,5))
-=======
+%=======
 title("Quaternions na situação de equilíbrio estável (Modelo não linear)")
 
 figure(2)
 plot(t1u,q1u(:,1))
->>>>>>> df85378a39f670aa288e4e30a1048dfd97ea968f
 hold on
 plot(t1u,q1u(:,2))
 hold on
@@ -57,7 +53,7 @@ plot(t1u,q1u(:,4))
 legend("q_0","q_1","q_2","q_3")
 xlabel("tempo (s)")
 ylabel("quaternions")
-title("Quaternions na situação de equilíbrio estável (Modelo não linear)")
+title("Quaternions na situação de equilíbrio instável (Modelo não linear)")
 
 figure(3)
 plot(t1s,q1s(:,5))
@@ -67,12 +63,9 @@ hold on
 plot(t1s,q1s(:,7))
 legend("w_x","w_y","w_z")
 xlabel("tempo (s)")
-<<<<<<< HEAD
 ylabel("Velocidade angular (rad/s)")
-=======
 ylabel("Velocidade ângular (rad/s)")
 title("Velocidade angular na situação de equilíbrio estável (Modelo não linear)")
->>>>>>> df85378a39f670aa288e4e30a1048dfd97ea968f
 
 figure(4)
 plot(t1u,q1u(:,5))
@@ -99,16 +92,3 @@ function dy = fnl(t,q)
     dy = [dy_1;dy_2;dy_3;dy_4;dy_5;dy_6;dy_7];
 end
 
-function dy = fnl(t,q)
-    T_x = 0;
-    T_y = 0;
-    T_z = 0;
-    dy_1 = 1/2*(-q(2)*q(5) - q(3)*q(6) - q(4)*q(7));
-    dy_2 = 1/2*(q(1)*q(5) - q(4)*q(6) + q(3)*q(7));
-    dy_3 = 1/2*(q(4)*q(5) + q(1)*q(6) - q(2)*q(7));
-    dy_4 = 1/2*(-q(3)*q(5) + q(2)*q(6) + q(1)*q(7));
-    dy_5 = 4875.31*(-0.00801894*q(1)^2 + 0.0160379*q(1)*q(2) + 0.00801894*q(2)^2 + 0.00801894*q(3)^2 + 0.0160379*q(3)*q(4) - 0.00801894*q(4)^2 + 0.027945*T_x + 0.012375*T_y + 0.012375*T_z) - 0.00102745*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2*q(6) + 0.234842*q(6)^2 + q(5)*(-0.00175975*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2 + 0.234842*q(6) - 0.234842*q(7)) - 0.00102745*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2*q(7) - 0.234842*q(7)^2;
-    dy_6 = 4875.31*(0.00801894*q(1)^2 - 0.00801894*q(2)^2 + 0.0160379*q(1)*q(3) - 0.00801894*q(3)^2 - 0.0160379*q(2)*q(4) + 0.00801894*q(4)^2 + 0.012375*T_x + 0.027945*T_y + 0.012375*T_z) - 0.234842*q(5)^2 + q(5)*(-0.00102745*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2 - 0.234842*q(6)) + q(6)*(-0.00175975*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2 + 0.234842*q(7)) - 0.00102745*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2*q(7) + 0.234842*q(7)^2;
-    dy_7 = 2437.65*(-0.0320758*q(1)*(q(2) + q(3)) + 0.0320758*q(2)*q(4) + 0.3*(-0.106919*q(3)*q(4) + 0.0825*(T_x + T_y)) + 0.04239*T_z) + 0.234842*q(5)^2 - 0.234842*q(6)^2 + q(6)*(-0.00102745*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2 - 0.234842*q(7)) + q(5)*(-0.00102745*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2 + 0.234842*q(7)) - 0.00175975*(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2)^2*q(7);
-    dy = [dy_1;dy_2;dy_3;dy_4;dy_5;dy_6;dy_7];
-end
